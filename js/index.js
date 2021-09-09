79b062b5341057e0843b83c1b76f9da04e86a42f
cynthia101488 <76106817+cynthia101488@users.noreply.github.com>
2021-09-09 11:24:08 +0800

    20210909 update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+const elemMode = document.querySelector('#Mode');
 const elemPage = document.querySelector('#Page');
 let pageArr = [];
 let currentIndex = 0;
 const dataSize = 10;
+let mode = 0;
 
 getData();
 setEvent();
@@ -13,7 +15,7 @@ function getData() {
     .then(data => {
       pageArr = pageSplit(data);
       setPage(pageArr);
-      setTemplate(currentIndex);
+      setTemplate(currentIndex, mode);
     })
 }
 
@@ -27,40 +29,96 @@ function pageSplit(data) {
 function setPage(arr) {
   let str = '';
   arr.forEach((item, index) => {
-    str += `<button class="btn" type="button" data-id="${index}">${index + 1}</button>`;  
+    str += `<button class="page__btn" type="button" data-id="${index}">${index + 1}</button>`;
   });
   elemPage.innerHTML = str;
-  elemPage.children[currentIndex].classList.add('js-btn');
+  elemPage.children[currentIndex].classList.add('js-page__btn');
 }
 
-function setTemplate(index) {
-  const elemListBox = document.querySelector('#List');
+function setTemplate(index, type) {
+  const elemList = document.querySelector('#List');
+  const elemTable = document.querySelector('#Table');
+  const elemTableBox = document.querySelector('#TableBox');
+  const elemCard = document.querySelector('#Card');
+  const elemCardBox = document.querySelector('#CardBox');
   const elemPageNum = document.querySelector('#PageNum');
   let str = '';
-  pageArr[index].forEach(item => {
-    str += `<div class="article__sec">
-              ${item.Url ? `<a class="article__link" href="${item.Url}" target="_blank">` : ''}
-                <div class="article__content">
+  switch (type) {
+    case 0:
+      elemList.style = 'display: block';
+      elemTableBox.style = 'display: none';
+      elemCardBox.style = 'display: none';
+      pageArr[index].forEach(item => {
+        str += `<div class="article__sec">
+                  ${item.Url ? `<a class="article__link" href="${item.Url}" target="_blank">` : ''}
+                    <div class="article__content">
+                      <div class="img__inner">
+                        <img class="image" src="${item.PicURL}" alt="${item.Name}">
+                      </div>
+                      <div class="article__info">
+                        <div class="article__head">
+                          <h2 class="article__tit">${item.Name}</h2>
+                        </div>
+                        <div class="article__body">
+                          <p class="article__desc">${textLimit(item.HostWords)}</p>
+                        </div>
+                      </div>
+                      <div class="article__footer">
+                        <h3 class="article__tag">${item.City}</h3>
+                        <h4 class="article__name">${item.Town}</h4>
+                      </div>
+                    </div>
+                  ${item.Url ? `</a>` : ''}
+                </div>`
+      });
+      elemList.innerHTML = str;
+      break;
+    case 1:
+      elemTableBox.style = 'display: block';
+      elemList.style = 'display: none';
+      elemCardBox.style = 'display: none';
+      const rowStartIndex = index * dataSize + 1;
+      pageArr[index].forEach((item, i) => {
+        str += `<tr class="table__ls ${changeBgColor(i)}">
+                  <td class="table__item text-right text-gray">${rowStartIndex + i}</td>
+                  <td class="table__item text-gray">${item.City}</td>
+                  <td class="table__item text-gray">${item.Town}</td>
+                  <td class="table__item">${item.Url ? `<a href="${item.Url}" target="_blank">${item.Name}</a>` : `${item.Name}`}</td>
+                  <td class="table__item text-overflow" title="${item.Address}">${item.Address}</td>
+                </tr>`;
+      });
+      elemTable.innerHTML = str;
+      break;
+    default:
+      elemCardBox.style = 'display: block';
+      elemList.style = 'display: none';
+      elemTableBox.style = 'display: none';
+      pageArr[index].forEach(item => {
+        str += `<div class="card__sec">
+              ${item.Url ? `<a class="card__link" href="${item.Url}" target="_blank">` : ''}
+                <div class="card__content">
                   <div class="img__inner">
                     <img class="image" src="${item.PicURL}" alt="${item.Name}">
                   </div>
-                  <div class="article__info">
-                    <div class="article__head">
-                      <h2 class="article__tit">${item.Name}</h2>
+                  <div class="card__info">
+                    <div class="card__head">
+                      <h3 class="card__tag">${item.City}</h3>
+                      <h4 class="card__name">${item.Town}</h4>
                     </div>
-                    <div class="article__body">
-                      <p class="article__desc">${textLimit(item.HostWords)}</p>
+                    <div class="card__body">
+                      <h2 class="card__tit">${item.Name}</h2>
                     </div>
-                  </div>
-                  <div class="article__footer">
-                    <h3 class="article__tag">${item.City}</h3>
-                    <h4 class="article__name">${item.Town}</h4>
+                    <div class="card__footer">
+                      <p class="card__desc">${textLimit(item.HostWords)}</p>
+                    </div>
                   </div>
                 </div>
               ${item.Url ? `</a>` : ''}
             </div>`
-  });
-  elemListBox.innerHTML = str;
+      });
+      elemCard.innerHTML = str;
+      break;
+  }
   elemPageNum.textContent = `美食頁次 ${index + 1}/14`;
 }
 
@@ -69,21 +127,42 @@ function textLimit(text) {
   return len > 100 ? text.substring(0, 100) + '...' : text;
 }
 
-function setEvent() {
-  elemPage.addEventListener('click', doClick);
+function changeBgColor(index) {
+  return index % 2 === 0 ? '' : 'js-table__ls';
 }
 
-function doClick(e) {
+function setEvent() {
+  elemMode.addEventListener('click', changeMode, true)
+  elemPage.addEventListener('click', changePage);
+}
+
+function changeMode(e) {
+  const self = e.target;
+  console.log(e);
+  if (self.nodeName === 'I') {
+    const prevMode = mode;
+    mode = parseInt(self.dataset.id);
+    setTemplate(currentIndex, mode);
+    setModeBtn(prevMode)
+  }
+}
+
+function setModeBtn(index) {
+  elemMode.children[mode].classList.add('js-nav__btn');
+  elemMode.children[index].classList.remove('js-nav__btn');
+}
+
+function changePage(e) {
   const self = e.target;
   if (self.nodeName === 'BUTTON') {
     const prevIndex = currentIndex;
     currentIndex = parseInt(self.dataset.id);
-    setTemplate(currentIndex);
-    setBtn(prevIndex);
+    setTemplate(currentIndex, mode);
+    setPageBtn(prevIndex);
   }
 }
 
-function setBtn(index) {
-  elemPage.children[currentIndex].classList.add('js-btn');
-  elemPage.children[index].classList.remove('js-btn');
+function setPageBtn(index) {
+  elemPage.children[currentIndex].classList.add('js-page__btn');
+  elemPage.children[index].classList.remove('js-page__btn');
 }
